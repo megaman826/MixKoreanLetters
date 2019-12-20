3adb3b8a28bee714870637af2259b6bccce4227b
megaman826 <megaman826@naver.com>
2019-12-20 16:52:24 +0900

    문장부호는 섞지 않는다.

--- MixKoreanLetters/MixKoreanLetters/Program.cs
+++ MixKoreanLetters/MixKoreanLetters/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace MixKoreanLetters
 {
@@ -25,52 +26,7 @@ namespace MixKoreanLetters
 
                 // cut 'text' by space
                 // relocate each words
-                text = text.Trim();
-                string[] lines = text.Split('\n');
-                string newLines = "";
-                Random random = new Random();
-                foreach(string line in lines)
-                {
-                    string newLine = "";
-                    string[] words = text.Split(' ');
-
-                    foreach(string word in words)
-                    {
-                        char[] array = word.ToCharArray();
-                        Random rng = new Random();
-                        int n = array.Length;
-                        int mixCount = n / 3;
-                        while (n > 1)
-                        {
-                            if(mixCount <= 0)
-                            {
-                                break;
-                            }
-                            mixCount--;
-
-                            n--;
-                            int k = rng.Next(n + 1);
-                            while(k == n)
-                            {
-                                k = rng.Next(n + 1);
-                            }
-
-                            if (array[k].Equals(',') || array[k].Equals('.')
-                                || array[n].Equals(',') || array[n].Equals('.'))
-                            {
-                                continue;
-                            }
-                            Console.WriteLine(array[k] + ">" + array[n]);
-
-                            var value = array[k];
-                            array[k] = array[n];
-                            array[n] = value;
-                        }
-                        newLine += new string(array) + " ";
-                    }
-
-                    newLines += newLine + "\n";
-                }
+                string newLines = MixLetters(ref text);
 
                 using (System.IO.StreamWriter file =
                     new System.IO.StreamWriter(resultFileName))
@@ -81,6 +37,54 @@ namespace MixKoreanLetters
                 Console.WriteLine(">>Make " + resultFileName + "!! Check This File.");
                 Console.WriteLine();
             }
+        }
+
+        private static string MixLetters(ref string text)
+        {
+            text = text.Trim();
+            string[] lines = text.Split('\n');
+            string newLines = "";
+            Random random = new Random();
+            foreach (string line in lines)
+            {
+                string newLine = "";
+                string[] words = text.Split(' ');
+
+                foreach (string word in words)
+                {
+                    char[] array = word.ToCharArray();
+                    Random rng = new Random();
+                    int n = array.Length;
+                    int mixCount = n / 3;
+                    while (n > 1)
+                    {
+                        if (mixCount <= 0)
+                        {
+                            break;
+                        }
+                        mixCount--;
+
+                        n--;
+                        int k = rng.Next(n + 1);
+                        while (k == n)
+                        {
+                            k = rng.Next(n + 1);
+                        }
+
+                        if (Char.IsPunctuation(array[k]) || Char.IsPunctuation(array[n]))
+                            continue;
+
+                        var value = array[k];
+                        array[k] = array[n];
+                        array[n] = value;
+                    }
+                    newLine += new string(array) + " ";
+                }
+
+                newLines += newLine + "\n";
+            }
+
+            return newLines;
         }
     }
 }
